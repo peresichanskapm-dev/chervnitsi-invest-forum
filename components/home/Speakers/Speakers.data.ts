4c606be8b03da3d83315f5357ccb102c05071eb7
Tochechka <zarytskyi.oleksandr.i@gmail.com>
2026-08-26 10:33:26 +0300

    feat(speakers): add Kozachuk, Iskierdo, Nepyivoda, Zhurzhii cards; move Korzhak after Klichuk

--- components/home/Speakers/Speakers.data.ts
+++ components/home/Speakers/Speakers.data.ts
@@ -28,6 +28,12 @@ export const speakersData = {
       description: "Чернівецький міський голова.",
     },
     {
+      firstName: "Роман",
+      lastName: "Коржак",
+      photo: "/images/speakers/korzhak.webp",
+      description: "СЕО компанії Blago.",
+    },
+    {
       firstName: "Яна",
       lastName: "Рогожа",
       photo: "/images/speakers/rohozha.webp",
@@ -46,12 +52,6 @@ export const speakersData = {
       photo: "/images/speakers/bachynskyi-taras.webp",
       description:
         "Інвест-директор та співзасновник MergeWave Capital, Bk invest. Інвестиційний радник понад 40 компаній України (Edem resort, МХП та ін.) та Європи. Понад 80+ інвестиційних та M&A угод. Викладач кількох MBA-шкіл (бізнес-школа УКУ, Львівська школа керівників бізнесу, IT Business school та інші) на тему злиття та поглинання, ментор Board й модератор інвест-комітету YBC.",
-    },
-    {
-      firstName: "Роман",
-      lastName: "Коржак",
-      photo: "/images/speakers/korzhak.webp",
-      description: "СЕО компанії Blago.",
     },
     {
       firstName: "Артур",
@@ -113,6 +113,33 @@ export const speakersData = {
       photo: "/images/speakers/kovalchuk.webp",
       description:
         "Founder & CEO інвестиційно-девелоперської компанії Alterra Group. Має інженерну й економічну освіти, пройшов програму Presidents' MBA в Києво-Могилянській бізнес-школі.",
+    },
+    {
+      firstName: "Мирослава",
+      lastName: "Козачук",
+      photo: "/images/speakers/kozachuk.webp",
+      description:
+        "Українська підприємниця, громадська діячка, інвестиційний банкір, власниця та СЕО компанії Franchise Group, трендвізіонерка франчайзингу в Україні, Східній Європі та Центральній Азії: 617+ створених франшиз, продано 6 200+ франшиз, виведення на міжнародний ринок 30+ українських франшиз. Авторка проєкту «Перша школа франчайзингу»: 530+ випускників власників великих мереж в Україні. Авторка проєкту Franchise Camp. Гордість — збільшення вартості франшизи в Україні від 3 000 до 250 000 $ за інтелектуальну складову. Створювала франшизи: Нова Пошта (Польща), Superhumans, Svarga, Sheriff, G.Bar, Salateira, Uklon, Gasanova, STEM-школа Inventor, Франсуа, Сімейна пекарня, Львівська майстерня шоколаду, сім'я ресторанів Дмитра Борисова, ЕкоЛавка, Бо Хліб, Універсум клінік, CSD, Big Money, заводи AKFA, IMZO (Узбекистан), Агромат, NAI, YBC, Зроби сам, Pizza Day.",
+    },
+    {
+      firstName: "Анна",
+      lastName: "Іскієрдо",
+      photo: "/images/speakers/iskierdo.webp",
+      description:
+        "Архітекторка, громадська діячка, співзасновниця та СЕО архітектурно-проєктної компанії АІММ. Голова правління Фонду Архітектурної палати України.",
+    },
+    {
+      firstName: "Володимир",
+      lastName: "Непийвода",
+      photo: "/images/speakers/nepyivoda.webp",
+      description:
+        "Співвласник та керуючий партнер YOD Group — компанії, що більше 20 років створює дизайн для ресторанів та готелів в Україні та за кордоном. Більше 200 реалізованих проєктів, 30 міжнародних відзнак. Buddha Bar New-York включений до топ-100 найкрасивіших ресторанів світу за рейтингом Grands Prix du Design від UNESCO. Проєкти у галузі рекреації: Hay Boutique Hotel and SPA by Edem Family, Mountain Residence, Emily Resort, Verholy Relax Park.",
+    },
+    {
+      firstName: "Андрій",
+      lastName: "Журжій",
+      photo: "/images/speakers/zhurzhii.webp",
+      description: "Засновник та СЕО Inzhur. Інвестор, підприємець, юрист.",
     },
   ] satisfies Speaker[],
 };
